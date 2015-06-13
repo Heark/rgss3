9cadc95676c344889abb5c10ef978f1f7427c1c5
Heark <hearkofsorcerey@gmail.com>
2015-06-13 19:39:52 -0400

    Create randommusic.rb

---
+++ randommusic.rb
@@ -0,0 +1,27 @@
+#==============================================================================
+# 
+#  Yuuta Kirishima / Dexter ~ Random Song PLayer
+# -- Script Call: RandomMusic.play(volume, pitch)
+# -- Example: RandomMusic.play(100, 100)
+#
+# 
+#==============================================================================
+
+$music = Dir["Audio/BGM/*"]
+
+class Music
+  attr_accessor :song
+  def initialize
+    @song = $music.sample
+  end
+  def play(volume, pitch)
+    RPG::BGM.new(@song, volume, pitch).play
+  end
+end
+RandomMusic = Music.new
+RandomMusic.song = $music.sample[10..500]
+
+
+
+
+  
