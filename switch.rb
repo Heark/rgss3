f81cc01bcb78308892aa32efbfdede8a99d54a4c
Heark <hearkofsorcerey@gmail.com>
2016-05-11 09:03:19 -0400

    Update switch.rb

--- switch.rb
+++ switch.rb
@@ -1,2 +1,11 @@
 #Beta Zeus Card Available
-$game_switches[11] = false
+$game_switches[11] = true
+if $game_switches[50] == false
+  $game_message.background = 1
+  $game_message.position = 1
+  $game_message.add("Beta Testing Zeus Card is now available, Talk to Steve to recieve your copy")
+  $game_switches[50] = true
+else 
+  
+end
+
